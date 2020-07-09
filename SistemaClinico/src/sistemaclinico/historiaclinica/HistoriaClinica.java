33cda1aa24842731ffbafef14782c1f30fb30ae6
carlosamr2 <66764349+carlosamr2@users.noreply.github.com>
2020-07-09 11:35:37 -0500

    historia clinica y cita añadido

---
+++ SistemaClinico/src/sistemaclinico/historiaclinica/HistoriaClinica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaclinico.historiaclinica;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Carlos
+ */
+public class HistoriaClinica {
+    protected int nro;
+    protected ArrayList enfermedades;
+    protected ArrayList alergias;
+    protected ArrayList otros;
+
+    public HistoriaClinica() {
+    }
+    
+}
