c782004102b910490893654439431cd6d32e4696
angtdiaz <nicoltuarezd@gmail.com>
2020-07-09 12:02:05 -0500

    Clase Pago

---
+++ SistemaClinico/src/sistemaclinico/pago/PagoEfectivo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaclinico.pago;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PagoEfectivo implements Pago {
+    protected float pago;
+
+    @Override
+    public boolean realizarPago(float monto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
