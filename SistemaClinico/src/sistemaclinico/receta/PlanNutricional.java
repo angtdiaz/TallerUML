ac0f56fd6a539811f3ac452e18c48366155fb4d6
carlosamr2 <66764349+carlosamr2@users.noreply.github.com>
2020-07-09 12:03:39 -0500

    area clinica y receta

---
+++ SistemaClinico/src/sistemaclinico/receta/PlanNutricional.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaclinico.receta;
+
+/**
+ *
+ * @author Carlos
+ */
+public class PlanNutricional {
+    protected String descripcion;
+
+    public PlanNutricional() {
+    }
+    
+}
---
+++ SistemaClinico/src/sistemaclinico/receta/Receta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaclinico.receta;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Receta {
+    protected Date fecha;
+
+    public Receta() {
+    }
+    
+}
